bd2dcb9926fabbd5402ad57e7b5eca1b15e13619
Renan0211 <renan.antunes.rocha@gmail.com>
2021-08-31 17:20:33 -0300

    resolve o desafio 1

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (req, res) => {
+  const talker = JSON.parse(fs.readFileSync('talker.json'));
+  if (talker.length === 0) return res.json([]);
+  res.json(talker);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
