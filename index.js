3450c420d63985f58f9537592d83d4265b6b1783
Renan0211 <renan.antunes.rocha@gmail.com>
2021-08-31 17:55:30 -0300

    resolve o segundo desafio

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ app.get('/talker', (req, res) => {
   res.json(talker);
 });
 
+app.get('/talker/:id', (req, res) => {
+  const talker = JSON.parse(fs.readFileSync('talker.json'));
+  const { id } = req.params;
+  const selectedTalker = talker.find((t) => t.id === parseInt(id, 10));
+  if (!selectedTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(selectedTalker);
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
