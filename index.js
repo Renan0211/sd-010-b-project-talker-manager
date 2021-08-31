161d673b587acee063e6b997f83f93a5b01bafce
Renan0211 <renan.antunes.rocha@gmail.com>
2021-08-31 19:39:29 -0300

    começa o desenvolvimento do req 3

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const randomToken = require('random-token');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+function validateEmail(email) {
+  // eslint-disable-next-line max-len
+  const validEmailShape = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!validEmailShape.test(email)) return 'unvalid email format';
+  if (!email || email === '') return 'undefined email';
+  return 'valid email';
+}
+
+function validatePassword(password) {
+  if (!password || password === '') return 'undefined password';
+  if (password.length < 6) return 'password too short';
+} 
 
 app.get('/talker', (req, res) => {
   const talker = JSON.parse(fs.readFileSync('talker.json'));
@@ -22,6 +36,22 @@ app.get('/talker/:id', (req, res) => {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   res.status(200).json(selectedTalker);
+});
+
+app.post('/login', (req, res) => {
+   const { email, password } = req.body;
+  if (validateEmail(email) === 'undefined email') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  } if (validateEmail(email) === 'unvalid email format') {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (validatePassword(password) === 'password too short') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (validatePassword(password) === 'undefined password') {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return res.status(200).json({ token: randomToken(16) });
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
